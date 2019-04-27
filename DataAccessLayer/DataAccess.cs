4c03a4a2098dbe079ce57cccae7df75d30089f5b
Anh Phan <anhphan.csf@gmail.com>
2019-04-27 16:00:29 +0700

    add data access object

---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataAccess
+    {
+        string connectionString = "Data Source=DESKTOP-PDSIPHP;Initial Catalog = MisaBook; Integrated Security = True";
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+
+        public DataAccess()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public bool CheckUserInfo(string username, string password)
+        {
+            sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = "[dbo].[Proc_CheckLoginInfo]";
+            sqlCommand.Parameters.AddWithValue("@Username", username);
+            sqlCommand.Parameters.AddWithValue("@Password", password);
+            bool res = (bool)sqlCommand.ExecuteScalar();
+            return res;
+        }
+    }
+}
