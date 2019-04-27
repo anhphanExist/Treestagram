4b7146803b3c52e6e0189dafec4f5ebde2e88e65
Anh Phan <anhphan.csf@gmail.com>
2019-04-27 16:02:55 +0700

    add access user object

---
+++ DataAccessLayer/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class User
+    {
+        DataAccess dataAccess;
+        public User()
+        {
+            dataAccess = new DataAccess();
+        }
+        public bool CheckUserLogin(string username, string password)
+        {
+            return dataAccess.CheckUserInfo(username, password);
+        }
+    }
+}
