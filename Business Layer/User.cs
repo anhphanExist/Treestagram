14207b4b5c404c3ffc9ce8f9c46f5f6cb64e2550
Anh Phan <anhphan.csf@gmail.com>
2019-04-27 16:10:55 +0700

    add user model

---
+++ Business Layer/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class User
+    {
+        DataAccessLayer.User accessUser;
+
+        public User()
+        {
+            accessUser = new DataAccessLayer.User();
+        }
+
+        public bool CheckUserLogin(string username, string password)
+        {
+            return accessUser.CheckUserLogin(username, password);
+        }
+    }
+}
---
+++ Treestagram/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Treestagram.Controllers
+{
+    public class UserController : ApiController
+    {
+        BusinessLogicLayer.User logicUser;
+
+        
+        [HttpGet]
+        public bool CheckInfoLogin(string username, string password)
+        {
+            logicUser = new BusinessLogicLayer.User();
+            return logicUser.CheckUserLogin(username, password);
+        }
+    }
+}
