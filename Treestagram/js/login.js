dde13acae236167e9f9e50ec7c0e24d9b78768a9
Anh Phan <anhphan.csf@gmail.com>
2019-05-05 15:54:07 +0700

    add login ajax

---
+++ Treestagram/js/login.js
@@ -0,0 +1,23 @@
+﻿$(document)ready(function () {
+    $("#login-btn").click(loginOnClick);
+});
+
+function loginOnClick() {
+    let username = $("#username-text-field").val().trim();
+    let password = $("#password-text-field").val().trim();
+    $.ajax({
+        method: "GET",
+        url: `/api/User?userName=${username}&passWord=${password}`,
+        success: function (response) {
+            if (response) {
+                window.location.href = "/views/newfeed.html";
+            }
+            else {
+                alert("Wrong username or password !!");
+            }
+        },
+        fail: function () {
+            alert("Please input your username and password");
+        }
+    });
+}
